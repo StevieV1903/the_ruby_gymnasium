b612ccd47277ec015cb747f2bff1a975e3f3108b
Steve Vance <steve.vance@hotmail.co.uk>
2020-08-17 10:46:09 +0100

    step 1 all pry removed

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -1,6 +1,5 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require ('pry')
 require_relative( '../models/member.rb' )
 also_reload('../models/*')
 
--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require ('pry')
 require_relative( '../models/session.rb' )
 also_reload('../models/*')
 
