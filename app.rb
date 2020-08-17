2e3bea18176aab634ba41c9dc52ccd913462430a
Steve Vance <steve.vance@hotmail.co.uk>
2020-08-17 10:50:36 +0100

    added if development to require contrib all in controllers and in app

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require( 'sinatra' )
-require( 'sinatra/contrib/all' )
+require( 'sinatra/contrib/all' ) if development?
 require_relative( 'controllers/bookings_controller' )
 require_relative( 'controllers/members_controller')
 require_relative( 'controllers/sessions_controller')
